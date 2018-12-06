e72010e11551a6eb6d699bde3e6e5536c0419a44
Elena <weiyaorui@gmail.com>
2018-12-06 12:21:33 -0800

    first commit

---
+++ src/main/java/intelli/spring5webapp/Elenaproject/ElenaprojectApplication.java
@@ -0,0 +1,12 @@
+package intelli.spring5webapp.Elenaproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElenaprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ElenaprojectApplication.class, args);
+	}
+}
---
+++ src/test/java/intelli/spring5webapp/Elenaproject/ElenaprojectApplicationTests.java
@@ -0,0 +1,16 @@
+package intelli.spring5webapp.Elenaproject;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ElenaprojectApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
